8c8b02c35504be24830abe82c7a4e8d87c72ef65
Gishnu Gompa <75305540+sproutcake23@users.noreply.github.com>
2025-04-03 21:52:32 +0530

    config changes

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,13 +2,37 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
+-- Font settings
 config.font = wezterm.font("CaskaydiaCove NFM")
-config.font_size = 16
--- add more config options above if you want
+config.font_size = 18
+config.line_height = 0.8
 
-config.enable_tab_bar = false
+-- Colors
+config.color_scheme = "Nocturnal Winter"
+config.default_cursor_style = "BlinkingBar"
+
+-- Background color and opacity
+config.colors = {
+	background = "#1e1e1e", -- Set your desired background color here
+	foreground = "#ffffff", -- You can also customize the foreground color
+}
+
+config.window_background_opacity = 0.55 -- Set the opacity to 80%
+
+-- Appearance
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0
-config.color_scheme = "AdventureTime"
-config.default_prog = { "powershell.exe" }
+config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+-- Miscellaneous settings
+config.max_fps = 144
+
+-- Set default shell to PowerShell 7
+config.default_prog = { "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe" }
+
 return config
